ea5d1772f93baefc2db27627c8e24ee299bfe5c1
Chuseok22 <bjh59629@naver.com>
2025-06-25 16:37:16 +0900

    first commit

---
+++ src/main/java/com/chuseok22/sejongportallogin/SejongPortalLoginApplication.java
@@ -0,0 +1,13 @@
+package com.chuseok22.sejongportallogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SejongPortalLoginApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SejongPortalLoginApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/chuseok22/sejongportallogin/SejongPortalLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuseok22.sejongportallogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SejongPortalLoginApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
